b972efcb21f3bdb7f1c42917b628647800dc3d2f
seomyeongjoo <mjseo0788@checkupv.com>
2023-03-14 21:21:06 +0900

    Initial commit

---
+++ src/main/java/com/numble/mybox/NumbleMyboxApplication.java
@@ -0,0 +1,13 @@
+package com.numble.mybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleMyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbleMyboxApplication.class, args);
+	}
+
+}
